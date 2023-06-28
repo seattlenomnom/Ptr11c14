64e95200b5a7fb2e8009129d60a3cfbce2b07e1a
seattlenomnom <mark.crapser@gmail.com>
2023-06-28 13:46:50 -0700

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for ptr11c14
+#
+ptr11c14: ptr11c14.c
+	gcc -g ptr11c14.c -o ./Build/ptr11c14
---
+++ ptr11c14.c
@@ -0,0 +1,55 @@
+/* source file for ptr11c14
+ *
+ * A demo program to show the use of increment/decrement operators on
+ * pointers to arrays.
+ *
+*/
+
+
+/* includes */
+
+
+
+
+
+
+
+
+
+
+/* defines */
+
+
+
+
+
+
+
+/* function declarations */
+
+
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+
+
+
+
+
+
+    return(0);
+}
+
+
+
+/* function definitions */
+
+
+
+
